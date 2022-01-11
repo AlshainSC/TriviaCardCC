2f0248a736b70cdebe455b66b7d3c0675d346737
AlshainSC <nicholas.m.larson@gmail.com>
2022-01-11 16:09:58 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+$(document).ready(() =>{
+    $('.hint-box').on('click', () => {
+      $('.hint').slideToggle(400);
+    });
+  
+    $('.wrong-answer-one').on('click', () => {
+  
+    });
+  
+    $('.wrong-answer-two').on('click', () => {
+  
+    });
+  
+    $('.wrong-answer-three').on('click', () => {
+  
+    });
+  });
