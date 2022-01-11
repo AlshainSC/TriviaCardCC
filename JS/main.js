4b2329963d73f4179f0cb2f106f6e4a799c9518d
AlshainSC <nicholas.m.larson@gmail.com>
2022-01-11 16:57:14 +0100

    finalize//still can add features

--- JS/main.js
+++ JS/main.js
@@ -4,14 +4,32 @@ $(document).ready(() =>{
     });
   
     $('.wrong-answer-one').on('click', () => {
-  
+        $('.wrong-text-one').fadeOut('slow');
+        $('.frown').show();
     });
   
     $('.wrong-answer-two').on('click', () => {
-  
+        $('.wrong-text-two').fadeOut('slow');
+        $('.frown').show();
     });
   
     $('.wrong-answer-three').on('click', () => {
-  
+        $('.wrong-text-three').fadeOut('slow');
+        $('.frown').show();
     });
+
+    $('.correct-answer').on('click', () => {
+        $('.frown').hide();
+        $('.smiley').show();
+        $('.correct-answer-text').fadeOut('slow');
+    });
+
+    $('.reset').on('click', () => {
+        $('.frown').hide();
+        $('.smiley').hide();
+        $('.wrong-text-one').fadeIn('fast');
+        $('.wrong-text-two').fadeIn('fast');
+        $('.wrong-text-three').fadeIn('fast');
+        $('.correct-answer-text').fadeIn('fast');
+    })
   });
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ In this project, you will follow step-by-step instructions to add jQuery Effects
 You will add a few different effects. A hint box will slide up and down. When you click an incorrect answer, it will fade away and a frowny face will appear. When you click the correct answer, the frowny face will disappear and a smiley face will take its place.
 
 To complete this project, you must know how to target HTML elements in jQuery, how to create event handlers, and how to use several jQuery Effects methods.
+
+
+STRETCH GOALS
+
+-->Add a button to reset the quiz and make the wrong answers reappear? /*done this*/
+-->Make a new question to appear after getting a question correct?
+-->Use arrays to set up a quiz with multiple questions?
